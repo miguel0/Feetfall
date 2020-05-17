56565f4ca4b948075dfcc8cca30d70ec7753d68e
PitayoSan <carloslopezels@gmail.com>
2020-05-16 21:40:40 -0500

    Add 'requires' verification for decision
    No wall JSON with stored decisions should contain the 'requires' section.
    With this feature, a player must have the required item in their
    inventory in order to have success in a choice.
    If the item is not present, the decision will lead to a failure state.

--- app/src/main/java/com/example/feetfall/utils/DecisionButton.java
+++ app/src/main/java/com/example/feetfall/utils/DecisionButton.java
@@ -1,16 +1,17 @@
 package com.example.feetfall.utils;
 
 public class DecisionButton {
-    public String text, result, success, failure;
+    public String text, result, requires, success, failure;
     public int exp, hp, str, def;
 
-    public DecisionButton(String text, String result, int exp, int hp, int str, int def, String success, String failure) {
+    public DecisionButton(String text, String result, int exp, int hp, int str, int def, String requires, String success, String failure) {
         this.text = text;
         this.result = result;
         this.exp = exp;
         this.hp = hp;
         this.str = str;
         this.def = def;
+        this.requires = requires;
         this.success = success;
         this.failure = failure;
     }
